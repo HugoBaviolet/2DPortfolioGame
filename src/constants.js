3f4b1d5fcef8b25e218797781b127e966e918603
Hugo A'Violet <hbaviolet@hotmail.co.uk>
2024-04-27 22:51:08 +0100

    Portfolio game

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. This is where I work on coding projects. I do not have a favourite language but I am enjoying gaining knowledge in Java, Javascript & Python. Here is my  <a href='https://www.github.com/hugobaviolet' target="_blank">GitHub</a>! & here is my <a href ='https://www.linkedin.com/in/hugo-aviolet/' target="_blank">LinkedIn</a>`,
+  "cs-degree": `This is my Computer Science Degree. I hung it on my wall because I'm proud of it. It was a great challenge and I was happy to increase my knowledge`,
+  "sofa-table": `That's my sofa. I like to relax and watch TV with my friends!`,
+  tv: `That's my TV, I enjoy watching Tech Youtubers and learning new skills in my free time.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and it has my resume (CV) on it <a href="https://github.com/HugoBaviolet/Resume/blob/main/Resume.pdf" target="_blank">Check it out?</a> Contact me at hbaviolet@hotmail.co.uk if you have any interesting job opportunities! `,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.`,
+  library: `There are a lot of programming books on my shelves. There are even some french books (I am learning french btw).
+  Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Hugo A'Violet's portfolio, just close the tab.`,
+};
---
+++ src/utils.js
@@ -0,0 +1,47 @@
+export function displayDialogue(text, onDisplayEnd) {
+  const dialogueUI = document.getElementById("textbox-container");
+  const dialogue = document.getElementById("dialogue");
+
+  // set display to block
+  dialogueUI.style.display = "block";
+
+  //text scrolling
+  let index = 0;
+  let currentText = "";
+  const intervalRef = setInterval(() => {
+    if (index < text.length) {
+      currentText += text[index];
+      dialogue.innerHTML = currentText;
+      index++;
+      return;
+    }
+    clearInterval(intervalRef);
+  }, 1);
+
+  const closeBtn = document.getElementById("close");
+
+  function onCloseBtnClick() {
+    onDisplayEnd();
+    dialogueUI.style.display = "none";
+    dialogue.innerHTML = "";
+    clearInterval(intervalRef);
+    closeBtn.removeEventListener("click", onCloseBtnClick);
+  }
+  //calls above function so we remove event listener since we add event listener when clicking
+  closeBtn.addEventListener("click", onCloseBtnClick);
+
+  addEventListener("keypress", (key) => {
+    if (key.code === "Enter") {
+      closeBtn.click();
+    }
+  });
+}
+
+export function setCamScale(k) {
+  const resizeFactor = k.width() / k.height();
+  if (resizeFactor < 1) {
+    k.camScale(k.vec2(1));
+  } else {
+    k.camScale(k.vec2(1.5));
+  }
+}
